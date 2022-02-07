885aef3b71c39b0fef61d5de979cb37d4d273bcb
sudipto-saha84 <sudiptosaha.diu84@gmail.com>
2022-02-07 17:34:02 +0600

    new

---
+++ app/classes/Example.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace App\classes;
+
+
+class Example
+{
+    public $i;
+
+
+public function index()
+{
+
+//    for ($this->i=45; $this->i>22; $this->i--)
+//    {
+//        echo $this->i. '<br/>';
+//
+//    }
+//    $this->i=10;
+//    while ($this->i<20)
+//    {
+//        echo $this->i;
+//        $this->i++;
+//    }
+//
+//    $this->i=10;
+//    do{
+//        echo $this->i. '<br/>';
+//        $this->i++;
+//    }
+//    while($this->i<30);
+
+    
+}
+}
